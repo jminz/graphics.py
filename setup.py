e647b4f1c91fe40e4c7f0886ee1e4bef501ad187
jminz <jminz.git@gmail.com>
2018-02-05 18:38:37 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(name='graphics',
+            version='0.1',
+            py_modules=['graphics'],
+            description='setup graphics',
+            author='none',
+            author_email='none',
+            url='localhost'
+           )
